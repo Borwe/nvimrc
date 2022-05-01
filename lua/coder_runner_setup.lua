0d23eeec1c5fce9de310007f4089695ed5ede414
brian.orwe <brian.orwe@gmail.com>
2022-05-01 12:27:09 +0300

    Update config

--- lua/coder_runner_setup.lua
+++ lua/coder_runner_setup.lua
@@ -4,7 +4,7 @@ M.setup = function ()
   require('code_runner').setup ({
     term = {
       position = "belowright",
-      size = 20
+      size = 13,
     },
     filetype_path = vim.fn.stdpath("config"):gsub("\\","/").."/codder.json",
   })
