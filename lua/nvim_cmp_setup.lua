1bad3d5cbc9b60c6e353cbe8a5b2fc2e2adec341
brian.orwe <brian.orwe@gmail.com>
2022-05-04 15:14:46 +0300

    Update nvim-cmp cmd

--- lua/nvim_cmp_setup.lua
+++ lua/nvim_cmp_setup.lua
@@ -10,12 +10,18 @@ M.setup = function()
             end
         },
         mapping = {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })
+          ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+          ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+          ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered()
         },
         sources = {
           { name = 'nvim_lsp' },
