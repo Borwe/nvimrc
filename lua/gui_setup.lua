75165af367b940960705a5051cb2876e2f6e09ae
brian.orwe <brian.orwe@gmail.com>
2024-01-15 22:09:25 +0300

    Remove font

--- lua/gui_setup.lua
+++ lua/gui_setup.lua
@@ -3,7 +3,7 @@ M = {}
 M.setup = function ()
     vim.g.neovide_cursor_vfx_mode="railgun"
     vim.g.remember_window_size = true
-    vim.o.guifont = "Intel One Mono Medium:h9"
+--vim.o.guifont = "Intel One Mono Medium:h9"
 end
 
 return M;
