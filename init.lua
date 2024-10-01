3ccc81270a4fc0fe6c5cbae94aed55f538726045
brian.orwe <brian.orwe@gmail.com>
2024-10-01 11:39:06 +0300

    Update nvm_cmp with codeium

--- init.lua
+++ init.lua
@@ -2,57 +2,58 @@
 local bootstrap = require('paq_setup').bootstrap
 -- Add packages
 bootstrap {
- 'nvim-lua/plenary.nvim', -- required by telescope
- 'tpope/vim-sleuth', -- for tabbing
- 'nvim-lualine/lualine.nvim', -- lualine
-  'nvim-tree/nvim-web-devicons',
- 'tpope/vim-fugitive',   --for git and info on airline
- 'neovim/nvim-lspconfig', -- for lsp
- 'leafgarland/typescript-vim', -- syntax highlighting for vim
- 'kyazdani42/nvim-tree.lua', -- for nvim-tree
--- START FOR COMPLETIONS
- 'hrsh7th/cmp-nvim-lsp',
- 'hrsh7th/cmp-buffer',
- 'hrsh7th/nvim-cmp',
- 'quangnguyen30192/cmp-nvim-ultisnips', -- for ulti snips completion
--- DONE FOR COMPLETIONS
--- START LspInstall
- "williamboman/mason.nvim",
- "williamboman/mason-lspconfig.nvim",
--- DONE LspInstall
- 'SirVer/ultisnips', -- for snippets
- 'mhinz/vim-startify', -- for managing startup and sessions
- {'nanotee/luv-vimdocs'}, --for luv documentation
- 'wsdjeg/luarefvim', -- for lua 5.1 documentation
- 'wakatime/vim-wakatime', -- wakatime
- 'olimorris/onedarkpro.nvim', -- theme
- {'Borwe/code_runner.nvim'}, -- code runner
- {'nvim-telescope/telescope.nvim', branch = '0.1.x'}, -- get telescope
- {'nvim-treesitter/nvim-treesitter'} -- setup treesitter
+    'nvim-lua/plenary.nvim',   -- required by telescope
+    'tpope/vim-sleuth',        -- for tabbing
+    'nvim-lualine/lualine.nvim', -- lualine
+    'nvim-tree/nvim-web-devicons',
+    'tpope/vim-fugitive',      --for git and info on airline
+    'neovim/nvim-lspconfig',   -- for lsp
+    'leafgarland/typescript-vim', -- syntax highlighting for vim
+    'kyazdani42/nvim-tree.lua', -- for nvim-tree
+    -- START FOR COMPLETIONS
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/nvim-cmp',
+    'Exafunction/codeium.nvim', -- codeium ai
+    'quangnguyen30192/cmp-nvim-ultisnips', -- for ulti snips completion
+    -- DONE FOR COMPLETIONS
+    -- START LspInstall
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    -- DONE LspInstall
+    'SirVer/ultisnips',                               -- for snippets
+    'mhinz/vim-startify',                             -- for managing startup and sessions
+    { 'nanotee/luv-vimdocs' },                        --for luv documentation
+    'wsdjeg/luarefvim',                               -- for lua 5.1 documentation
+    'wakatime/vim-wakatime',                          -- wakatime
+    'catppuccin/nvim',
+    { 'Borwe/code_runner.nvim' },                     -- code runner
+    { 'nvim-telescope/telescope.nvim',  branch = '0.1.x' }, -- get telescope
+    { 'nvim-treesitter/nvim-treesitter' }             -- setup treesitter
 }
 require('gui_setup').setup()
 require("tree_sitter_setup").setup()
 
 -- valriables
-local scopes={o=vim.o,b=vim.bo,w=vim.wo}
+local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
 
 -- For setting options that require values
-local function opts_with_val(scope,key,value)
-    scopes[scope][key]=value
+local function opts_with_val(scope, key, value)
+    scopes[scope][key] = value
 end
 
 -- Create mappings
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then
-      options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- For seting options
 local function opts(key)
-    vim.cmd("set "..key)
+    vim.cmd("set " .. key)
 end
 
 -- setup nvim-cmp
@@ -61,7 +62,7 @@ require('nvim_cmp_setup').setup()
 --require('coder_runner_setup').setup()
 --setup nvim-tree
 require('nvim-tree').setup({
-  update_cwd=true
+    update_cwd = true
 })
 --setup mason
 require('mason').setup()
@@ -71,14 +72,14 @@ require('telescope_setup').setup()
 require('lsp_pers_config')
 
 -- setup some configurations
-opts_with_val('o','background','dark') -- Set background
+opts_with_val('o', 'background', 'dark')                                                                      -- Set background
 vim.cmd('au BufReadPost * if line("\'\\"") > 1 && line("\'\\"") <= line("$") | exe "normal! g\'\\"" | endif') -- Open file in last location
 opts("showcmd")
 -- setup lualine
 require('lualine').setup()
 -- setup the background theme
-vim.o.background="light"
-require('onedarkpro').load()
+vim.o.background = "light"
+vim.cmd("colorscheme catppuccin-mocha")
 opts("showmatch")
 opts("ignorecase")
 opts("incsearch")
@@ -89,40 +90,40 @@ opts("autoread")
 opts("expandtab")
 opts("smartindent")
 opts("wildmenu")
-vim.api.nvim_set_option('encoding','utf-8')
-vim.api.nvim_set_option('tabstop',2)
-vim.api.nvim_set_option('tabstop',2)
-vim.api.nvim_set_option('softtabstop',2)
-vim.api.nvim_set_option('shiftwidth',2)
-vim.api.nvim_set_option('foldmethod','syntax')
-vim.api.nvim_set_option('backspace','2')
-vim.api.nvim_set_option('path',vim.o.path.."**")
+vim.api.nvim_set_option('encoding', 'utf-8')
+vim.api.nvim_set_option('tabstop', 2)
+vim.api.nvim_set_option('tabstop', 2)
+vim.api.nvim_set_option('softtabstop', 2)
+vim.api.nvim_set_option('shiftwidth', 2)
+vim.api.nvim_set_option('foldmethod', 'syntax')
+vim.api.nvim_set_option('backspace', '2')
+vim.api.nvim_set_option('path', vim.o.path .. "**")
 
 -- mappings
 
-vim.api.nvim_create_user_command("PTestFile", require('testing_mod').test_current_file, {['bang']=true})
-map('n','vrc',
-':edit '..vim.fn.stdpath('config')..'/init.lua<CR>')
-map('n','pch',':tchdir '..vim.fn.stdpath('data')..'/site/pack/paqs/start<CR>')
-map('n','vch',':tchdir '..vim.fn.stdpath('config')..'<CR>')
+vim.api.nvim_create_user_command("PTestFile", require('testing_mod').test_current_file, { ['bang'] = true })
+map('n', 'vrc',
+    ':edit ' .. vim.fn.stdpath('config') .. '/init.lua<CR>')
+map('n', 'pch', ':tchdir ' .. vim.fn.stdpath('data') .. '/site/pack/paqs/start<CR>')
+map('n', 'vch', ':tchdir ' .. vim.fn.stdpath('config') .. '<CR>')
 --setup terminal
 require("term_handler").setup()
 
 -- for quotes and other double entries
-map('i','"','""<C-[>i')
-map('i','\'','\'\'<C-[>i')
-map('i','{','{}<C-[>i')
-map('i','[','[]<C-[>i')
-map('i','(','()<C-[>i')
+map('i', '"', '""<C-[>i')
+map('i', '\'', '\'\'<C-[>i')
+map('i', '{', '{}<C-[>i')
+map('i', '[', '[]<C-[>i')
+map('i', '(', '()<C-[>i')
 
 -- for resizing
-map('n','rs+','<cmd>vertical resize +5<CR>')
-map('n','rs-','<cmd>vertical resize -5<CR>')
-map('n','vrs+','<cmd>resize +5<CR>')
-map('n','vrs-','<cmd>resize -5<CR>')
+map('n', 'rs+', '<cmd>vertical resize +5<CR>')
+map('n', 'rs-', '<cmd>vertical resize -5<CR>')
+map('n', 'vrs+', '<cmd>resize +5<CR>')
+map('n', 'vrs-', '<cmd>resize -5<CR>')
 
 -- for nerdtree
-map('n','ntree','<cmd>NvimTreeToggle<CR>')
+map('n', 'ntree', '<cmd>NvimTreeToggle<CR>')
 -- Set the tab cmd
 require('tab_prompt')
 
@@ -131,8 +132,8 @@ require('buffer_prompt')
 
 
 -- Set completeopt
-opts_with_val('o','completeopt','menuone,noinsert,noselect')
-opts_with_val('o','shortmess',vim.o.shortmess..'c')
+opts_with_val('o', 'completeopt', 'menuone,noinsert,noselect')
+opts_with_val('o', 'shortmess', vim.o.shortmess .. 'c')
 
 -- make file with .sol be detected as solidity filetype
 vim.cmd("autocmd BufRead *.sol exec 'set filetype=solidity'")
--- lua/nvim_cmp_setup.lua
+++ lua/nvim_cmp_setup.lua
@@ -1,6 +1,5 @@
 local cmp = require('cmp')
-
-M = {}
+require('codeium').setup({})
 
 M.setup = function()
     cmp.setup({
@@ -10,29 +9,33 @@ M.setup = function()
             end
         },
         mapping = {
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-          ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.close(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+            ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+            ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
         },
         window = {
             completion = cmp.config.window.bordered(),
             documentation = cmp.config.window.bordered()
         },
         sources = {
-          { name = 'nvim_lsp' },
+            { name = 'nvim_lsp' },
 
-          -- For ultisnips user.
-          { name = 'ultisnips' },
+            -- For ultisnips user.
+            { name = 'ultisnips' },
 
-          { name = 'buffer' },
+            { name = 'buffer' },
+
+            -- For codeium
+            { name = 'codeium' },
         }
     })
 end
 
-return M
+-- function that prints hello world
 
+return M
