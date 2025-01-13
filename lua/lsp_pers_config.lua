de319c18979f6dcc5a14cf391ff0a40a97abbb63
brian.orwe <brian.orwe@gmail.com>
2025-01-13 19:02:02 +0300

    Remove depricated functions

--- lua/lsp_pers_config.lua
+++ lua/lsp_pers_config.lua
@@ -2,41 +2,41 @@ local lspconfig = require('lspconfig')
 local lspconfig_configurer = require('lspconfig.configs')
 
 -- Configure to work with Ultisnips templates
-vim.g.completion_enable_snippet='UltiSnips'
+vim.g.completion_enable_snippet = 'UltiSnips'
 
 
 -- Configure for buffers complete
-vim.g.completion_chain_complete_list={
-    {complete_items= {'lsp'}},
-    {complete_items = {'buffers'}},
-    {mode = { '<c-p>' }},
-    {mode = { '<c-n>' }}
+vim.g.completion_chain_complete_list = {
+    { complete_items = { 'lsp' } },
+    { complete_items = { 'buffers' } },
+    { mode = { '<c-p>' } },
+    { mode = { '<c-n>' } }
 }
 
-local map= function (type,key,value)
-    vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap=true, silent=true});
+local map = function(type, key, value)
+    vim.api.nvim_buf_set_keymap(0, type, key, value, { noremap = true, silent = true });
 end
 
 -- for renaming using lsp
-vim.api.nvim_create_user_command('LspRename', vim.lsp.buf.rename, {['bang']=true})
+vim.api.nvim_create_user_command('LspRename', vim.lsp.buf.rename, { ['bang'] = true })
 
 -- Handle mapping and execution once LSP is attatched
-local custom_on_attach_lsp=function (client)
-    print('LSP '..client.name..' started')
+local custom_on_attach_lsp = function(client)
+    print('LSP ' .. client.name .. ' started')
 
     -- Set key mappings
-    map('n','<Space>n','<cmd>lua vim.lsp.buf.definition()<CR>')
-    map('n','<Space>k','<cmd>lua vim.lsp.buf.hover()<CR>')
-    map('n','<Space>a','<cmd>lua vim.lsp.buf.hover();vim.lsp.buf.hover()<CR>')
-    map('n','<Space>i','<cmd>lua vim.lsp.buf.implementation()<CR>')
-    map('n','<Space>s','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-    map('n','<Space>d','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-    map('n','<Space>r','<cmd>lua vim.lsp.buf.references()<CR>')
-    map('n','<Space>q','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-    map('n','<Space>w','<cmd>lua vim.lsp.buf.format()<CR>')
-    map('n','<Space>m','<cmd>lua vim.lsp.buf.declaration()<CR>')
-    map('n','<Space>x','<cmd>lua vim.diagnostic.open_float()<CR>')
-    map('n','<Space>f','<cmd>lua vim.lsp.buf.code_action()<CR>')
+    map('n', '<Space>n', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    map('n', '<Space>k', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    map('n', '<Space>a', '<cmd>lua vim.lsp.buf.hover();vim.lsp.buf.hover()<CR>')
+    map('n', '<Space>i', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+    map('n', '<Space>s', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    map('n', '<Space>d', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+    map('n', '<Space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+    map('n', '<Space>q', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+    map('n', '<Space>w', '<cmd>lua vim.lsp.buf.format()<CR>')
+    map('n', '<Space>m', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+    map('n', '<Space>x', '<cmd>lua vim.diagnostic.open_float()<CR>')
+    map('n', '<Space>f', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 end
 
 
@@ -45,45 +45,45 @@ end
 
 -- setup nuru-lsp
 lspconfig_configurer["nuru-lsp"] = {
-  default_config = {
-    cmd = { 'C:/Users/Brian/Workspaces/nuru-lsp/nuru-lsp.exe', 'C:/Users/Brian/Workspaces/nuru-lsp/lsp.log' },
-    filetypes = { 'sr', 'nroff' },
-    root_dir = require('lspconfig.util').find_git_ancestor,
-    single_file_support = true,
-  },
-  docs = {
-    description = [[
+    default_config = {
+        cmd = { 'C:/Users/Brian/Workspaces/nuru-lsp/nuru-lsp.exe', 'C:/Users/Brian/Workspaces/nuru-lsp/lsp.log' },
+        filetypes = { 'sr', 'nroff' },
+        root_dir = require('lspconfig.util').find_git_ancestor,
+        single_file_support = true,
+    },
+    docs = {
+        description = [[
 https://github.com/Borwe/nuru-lsp
 
 Nuru Unofficial Language Server
         ]],
-    default_config = {
-      root_dir = lspconfig.util.find_git_ancestor,
+        default_config = {
+            root_dir = lspconfig.util.find_git_ancestor,
+        },
     },
-  },
 }
-lspconfig["nuru-lsp"].setup{
+lspconfig["nuru-lsp"].setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
         .make_client_capabilities()),
-    on_attach=custom_on_attach_lsp
+    on_attach = custom_on_attach_lsp
 }
 
 require('mason-lspconfig').setup {
-    ensure_installed = {"lua_ls"},
+    ensure_installed = { "lua_ls", "clangd", "rust_analyzer", "zls", "ts_ls" },
     handlers = {
-        function (server_name)
+        function(server_name)
             lspconfig[server_name].setup {
                 capabilities = require('cmp_nvim_lsp')
-                .default_capabilities(vim.lsp.protocol
-                    .make_client_capabilities()),
-                on_attach=custom_on_attach_lsp
+                    .default_capabilities(vim.lsp.protocol
+                        .make_client_capabilities()),
+                on_attach = custom_on_attach_lsp
             }
         end,
         ["lua_ls"] = function()
             lspconfig.lua_ls.setup {
-                capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
-                    .make_client_capabilities()),
-                on_attach=custom_on_attach_lsp,
+                capabilities = require('cmp_nvim_lsp').
+                    default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+                on_attach = custom_on_attach_lsp,
                 settings = {
                     Lua = {
                         runtime = {
@@ -97,8 +97,8 @@ require('mason-lspconfig').setup {
                             },
                         },
                         diagnostics = {
-                            globals = {"vim"},
-                            disable = {"lowercase-global", "unused-function"},
+                            globals = { "vim" },
+                            disable = { "lowercase-global", "unused-function" },
                         },
                     },
                 },
